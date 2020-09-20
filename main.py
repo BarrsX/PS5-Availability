9c3f4de06ea2dd752a08fa1e693d23445766cd15
Xavion Barrs <xavionbarrs@gmail.com>
2020-09-19 20:40:52 -0400

    Added more websites and DataFrame.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from plyer import notification
+import pandas as pd
+
+data = {'Amazon': ['No'],
+        'Best Buy': ['No'],
+        'WalMart': ['No']}
+
+df = pd.DataFrame(data, columns=['Amazon', 'Best Buy', 'WalMart'])
+print(df)
 
 headers = ({'User-Agent':
                 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 '
@@ -22,12 +30,49 @@ amazon = Page(
     'https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG/ref=sr_1_1?dchild=1&keywords=ps5&qid=1600547494&sr=8-1&th=1')
 ps5 = amazon.req()
 avail = ps5.find("span", attrs={"class": 'a-size-medium a-color-price'})
-avail_value = avail.string
+amazon_value = avail.string
 
-if (avail_value.strip()) == 'Currently unavailable.':
+if (amazon_value.strip()) != 'Currently unavailable.':
     notification.notify(
         title='PS5 Available',
         message='A PS5 is available on Amazon',
         app_icon=None,
         timeout=10,
     )
+    df['Amazon'] = df['Amazon'.replace('No', 'Yes')]
+else:
+    df['Amazon'] = df['Amazon'.replace('No', 'Yes')]
+
+bestbuy = Page('https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161')
+bb = bestbuy.req()
+bbAvail = bb.find("button", attrs={'class': 'btn btn-disabled btn-lg btn-block add-to-cart-button'})
+bb_value = bbAvail
+
+if (bb_value.strip()) != 'Coming Soon':
+    notification.notify(
+        title='PS5 Available',
+        message='A PS5 is available on Best Buy',
+        app_icon=None,
+        timeout=10,
+    )
+    df['Best Buy'] = df['Best Buy'.replace('No', 'Yes')]
+else:
+    df['Best Buy'] = df['Best Buy'.replace('No', 'Yes')]
+
+walmart = Page('https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815')
+wm = walmart.req()
+walmartAvail = wm.find("span", attrs={'class': 'display-block-xs font-bold'})
+wm_value = walmartAvail
+
+if (wm_value.strip()) != 'Out of stock':
+    notification.notify(
+        title='PS5 Available',
+        message='A PS5 is available on WalMart',
+        app_icon=None,
+        timeout=10,
+    )
+    df['WalMart'] = df['WalMart'.replace('No', 'Yes')]
+else:
+    df['WalMart'] = df['WalMart'.replace('No', 'Yes')]
+
+print(df)
