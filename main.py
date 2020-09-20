84da570f0d82bcf7852cc26a731b7732e391fb20
Xavion Barrs <xavionbarrs@gmail.com>
2020-09-20 12:30:54 -0400

    Debugged Best Buy and Walmart stock checker.

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ data = {'Amazon': ['No'],
         'WalMart': ['No']}
 
 df = pd.DataFrame(data, columns=['Amazon', 'Best Buy', 'WalMart'])
-print(df)
+# print(df)
 
 headers = ({'User-Agent':
-                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 '
+                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 '
                 'Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
@@ -27,7 +27,8 @@ class Page:
 
 
 amazon = Page(
-    'https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG/ref=sr_1_1?dchild=1&keywords=ps5&qid=1600547494&sr=8-1&th=1')
+    'https://www.amazon.com/PlayStation-5-Console/dp/B08FC5L3RG/ref=sr_1_1?dchild=1&keywords=ps5&qid=1600547494&sr=8'
+    '-1&th=1')
 ps5 = amazon.req()
 avail = ps5.find("span", attrs={"class": 'a-size-medium a-color-price'})
 amazon_value = avail.string
@@ -39,14 +40,14 @@ if (amazon_value.strip()) != 'Currently unavailable.':
         app_icon=None,
         timeout=10,
     )
-    df['Amazon'] = df['Amazon'.replace('No', 'Yes')]
+    df['Amazon'] = df['Amazon'].replace({'No': 'Yes'})
 else:
-    df['Amazon'] = df['Amazon'.replace('No', 'Yes')]
+    df['Amazon'] = df['Amazon'].replace({'Yes': 'No'})
 
 bestbuy = Page('https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161')
 bb = bestbuy.req()
 bbAvail = bb.find("button", attrs={'class': 'btn btn-disabled btn-lg btn-block add-to-cart-button'})
-bb_value = bbAvail
+bb_value = bbAvail.string
 
 if (bb_value.strip()) != 'Coming Soon':
     notification.notify(
@@ -55,14 +56,14 @@ if (bb_value.strip()) != 'Coming Soon':
         app_icon=None,
         timeout=10,
     )
-    df['Best Buy'] = df['Best Buy'.replace('No', 'Yes')]
+    df['Best Buy'] = df['Best Buy'].replace({'No': 'Yes'})
 else:
-    df['Best Buy'] = df['Best Buy'.replace('No', 'Yes')]
+    df['Best Buy'] = df['Best Buy'].replace({'Yes': 'No'})
 
 walmart = Page('https://www.walmart.com/ip/Sony-PlayStation-5-Digital-Edition/493824815')
 wm = walmart.req()
 walmartAvail = wm.find("span", attrs={'class': 'display-block-xs font-bold'})
-wm_value = walmartAvail
+wm_value = walmartAvail.string
 
 if (wm_value.strip()) != 'Out of stock':
     notification.notify(
@@ -71,8 +72,8 @@ if (wm_value.strip()) != 'Out of stock':
         app_icon=None,
         timeout=10,
     )
-    df['WalMart'] = df['WalMart'.replace('No', 'Yes')]
+    df['WalMart'] = df['WalMart'].replace({'No': 'Yes'})
 else:
-    df['WalMart'] = df['WalMart'.replace('No', 'Yes')]
+    df['WalMart'] = df['WalMart'].replace({'Yes': 'No'})
 
 print(df)
